1aa29a1f39afd3b4cb8f37763b7a2c2fbec73fbd
cyamy <hi.cyamy@gmail.com>
2023-05-27 19:33:39 +0900

    add example

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,67 @@
 import Didact from "./didact";
 
+type FetchData = {
+	isLoaded: boolean;
+	user: {
+		userId: number;
+		id: number;
+		title: string;
+		completed: boolean;
+	};
+};
+
+const initialData = {
+	isLoaded: false,
+	user: {
+		userId: 0,
+		id: 0,
+		title: "initial title",
+		completed: false,
+	},
+};
+
 /** @jsx Didact.createElement */
-function Counter() {
-	const [state, setState] = Didact.useState(1);
+function App() {
+	const [count, setCount] = Didact.useState(1);
+	const [data, setData] = Didact.useState<FetchData>(initialData);
+
+	const handleCountUp = () => setCount((prev) => prev + 1);
+	const handleCountDown = () => setCount((prev) => prev - 1);
+
+	Didact.useEffect(async () => {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/todos/1",
+		);
+		const result = await response.json();
+		setData(() => {
+			return {
+				isLoaded: true,
+				user: result,
+			};
+		});
+	}, []);
+
 	// TODO: jsx factory の調査・エラー対応
-	// @ts-expect-error
-	// rome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-	return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+	return (
+		<div>
+			<p>Count: {count}</p>
+			<button onClick={handleCountUp}>count up</button>
+			<button onClick={handleCountDown}>count down</button>
+			<hr />
+			{data.isLoaded ? (
+				<div>
+					<p>user info</p>
+					<p>id: {data.user.id}</p>
+					<p>{data.user.title}</p>
+				</div>
+			) : (
+				<p>loading...</p>
+			)}
+		</div>
+	);
 }
 
 // @ts-expect-error
-const element = <Counter />;
+const element = <App />;
 const container = document.getElementById("root") as HTMLElement;
 Didact.render(element, container);
